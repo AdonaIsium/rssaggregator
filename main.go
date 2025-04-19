f766446c732c1907c883f64cac91a3743a22b8d3
Lanre <lanre@Haliax0.local>
2025-04-19 14:16:53 -0500

    added get feeds endpoint

--- main.go
+++ main.go
@@ -59,7 +59,9 @@ func main() {
 	v1Router.Get("/err", handlerError)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
 	r.Mount("/v1", v1Router)
 
