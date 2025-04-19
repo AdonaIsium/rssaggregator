e68e100b89d25d315e1f4f2d2ba1fd3728f231c7
Lanre <lanre@Haliax0.local>
2025-04-19 13:24:07 -0500

    added api keys and ability to perform GET requests on users by API key

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/AdonaIsium/rssaggregator/internal/auth"
 	"github.com/AdonaIsium/rssaggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -33,5 +34,20 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		respondWithError(w, 400, fmt.Sprintf("yikes, user not able to be created~: %v", err))
 		return
 	}
-	respondWithJSON(w, 200, user)
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("couldn't get user: %v", err))
+	}
+
+	respondWithJSON(w, 200, databaseUserToUser(user))
 }
